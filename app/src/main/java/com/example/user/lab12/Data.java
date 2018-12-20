fcb0da26d9920cd328170f1fbdea2812b914eac4
user <willy21616@gmail.com>
2018-12-20 14:48:40 +0800

    Lab12

---
+++ app/src/main/java/com/example/user/lab12/Data.java
@@ -0,0 +1,14 @@
+package com.example.user.lab12;
+
+import javax.xml.transform.Result;
+
+public class Data {
+    Result result;
+    class Result{
+        Results[] results;
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
